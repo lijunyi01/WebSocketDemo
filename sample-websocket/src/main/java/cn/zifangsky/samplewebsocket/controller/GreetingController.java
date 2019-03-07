e416e9517965725482481ede1bf946ab7955c278
lijunyi01 <lijunyi01@gmail.com>
2019-03-07 16:44:00 +0800

    modify

--- sample-websocket/src/main/java/cn/zifangsky/samplewebsocket/controller/GreetingController.java
+++ sample-websocket/src/main/java/cn/zifangsky/samplewebsocket/controller/GreetingController.java
@@ -13,20 +13,20 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class GreetingController {
 
-    // 浏览器访问：http://localhost:7000/reverse
-    @RequestMapping("/reverse")
-    public ModelAndView reverse(){
+    // 浏览器访问：http://localhost:7000/reverse-show , 然后在浏览器console里看服务端推送的结果
+    @RequestMapping("/reverse_show")
+    public ModelAndView reverse_show(){
         return new ModelAndView("websocket/reverse");
     }
 
-    // 浏览器访问：http://localhost:7000/echo
-    @RequestMapping("/echo")
-    public ModelAndView echo(){
+    // 浏览器访问：http://localhost:7000/echo_show
+    @RequestMapping("/echo_show")
+    public ModelAndView echo_show(){
         return new ModelAndView("websocket/echo");
     }
 
-    @RequestMapping("/echo_sockjs")
-    public ModelAndView echoSockJS(){
+    @RequestMapping("/echo_sockjs_show")
+    public ModelAndView echoSockJS_show(){
         return new ModelAndView("websocket/echo_sockjs");
     }
 
